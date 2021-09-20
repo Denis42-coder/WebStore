f45cfa59ea90c1ae4add1416de07fd49a73de8a9
Denis <actdv@yandex.ru>
2021-09-20 18:21:18 +0300

    файл конфигурации

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -47,11 +48,15 @@ namespace WebStore
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            //app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                //var grettings = configuration["Greetings"];//"Hi!";
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync(Configuration["Greetings"]);
+                });
             });
         }
     }
