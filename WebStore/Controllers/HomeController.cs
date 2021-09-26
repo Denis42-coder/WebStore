301d1c20d0bf9fb694e0d7a41ce4a88e702b19ca
Denis <actdv@yandex.ru>
2021-09-26 16:13:54 +0300

    Доделал 1 урок

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
@@ -10,9 +11,9 @@ namespace WebStore.Controllers
     {
         private static readonly List<Employee> __Employees = new()
         {
-            new Employee { Id = 1, LastName = "Иванов", FirstName ="Иван", Papronymic = "Иванович", Age = 159},
-            new Employee { Id = 2, LastName = "Петров", FirstName = "Федор", Papronymic = "Иванович", Age = 19 },
-            new Employee { Id = 3, LastName = "Сидлоров", FirstName = "Петр", Papronymic = "Иванович", Age = 59 },
+            new Employee { Id = 1, LastName = "Иванов", FirstName ="Иван", Patronymic = "Иванович", Age = 159},
+            new Employee { Id = 2, LastName = "Петров", FirstName = "Федор", Patronymic = "Иванович", Age = 19 },
+            new Employee { Id = 3, LastName = "Сидлоров", FirstName = "Петр", Patronymic = "Иванович", Age = 59 },
         };
         public IActionResult Index()// localhost:5000/Home/Index
         {
@@ -24,7 +25,7 @@ namespace WebStore.Controllers
             return Content($"SecondAction param {id}");
         }
 
-        public IActionResult Employees()// localhost:5000/Home/Employees
+        public IActionResult Employees()// localhost:5001/Home/Employees
         {
             return View(__Employees);
         }
